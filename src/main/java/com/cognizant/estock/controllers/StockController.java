bf9d57901d4ff66b020184249de1b08686b7bd5a
cTre19 <juancarlostrejo19@yahoo.com>
2021-07-24 03:16:35 -0500

    fixed cross origin annotation

--- src/main/java/com/cognizant/estock/controllers/StockController.java
+++ src/main/java/com/cognizant/estock/controllers/StockController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RequestMapping("/api/v1.0/market/stock")
 public class StockController {
 
